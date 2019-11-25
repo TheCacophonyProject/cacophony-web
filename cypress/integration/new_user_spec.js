b7935d3f712cd3e3c2f13216b1d805db18cace73
Clare McLennan <57052+clare@users.noreply.github.com>
2019-11-25 14:27:12 +1300

    Initial commit for integration tests

---
+++ cypress/integration/new_user_spec.js
@@ -0,0 +1,40 @@
+var crypto = require("crypto");
+
+context('New User', () => {
+  var uniqueId =  crypto.randomBytes(2).toString('hex');
+  var username = "integrationtester"  +  uniqueId;
+  var password = 'p' + username;
+  var group = 'test' + uniqueId;
+
+  beforeEach(() => {
+    cy.visit('')
+  })
+
+  it('A new user can create an account, and a group', () => {
+    // create account
+    cy.contains("Register here").click();
+    cy.contains("Username").siblings().find("input").type(username);
+    cy.contains("Email").siblings().type(username + "@fake.address.com");
+    cy.contains("Password").siblings().type(password);
+    cy.contains("Retype password").siblings().type(password);
+    cy.get("button").contains("Register").click();
+
+    // create new group
+    cy.location({timeout: 60000}).should((location) => {expect(location.pathname).to.equal('/');});
+    cy.contains("It looks like you're new here");
+    cy.contains("Create a group").click();
+    cy.contains("Group name").parent().type(group);
+    cy.contains("Create new group").click();
+
+    // check new group is created properly
+    cy.location({timeout: 60000}).should((location) => {expect(location.pathname).contains(group);});
+    cy.contains('Group: ' + group);
+    // user is an administrator for group
+  });
+
+  it('New user can sign in and create a group', () => {
+    cy.signin(username, password);
+  });
+})
+
+
