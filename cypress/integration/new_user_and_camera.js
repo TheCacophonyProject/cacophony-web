256136f2c68ccc8942e3f5bbd841e8fbd79985b1
clare <57052+clare@users.noreply.github.com>
2020-01-23 09:25:08 +1300

    Integration checks now create a camera and check user can see it.
    * Added readme with how to run
    
    * test run tests
    
    * Fail travis check
    
    * Reset to working tests
    
    * Checking whether cypress records in integration test runner
    
    * Small change to trigger rebuild
    
    * Tests should now use the fake-thermal-camera to test a camera joining a new group.
    
    * Fix error for starting fake-camera
    
    * Hopefully fix popup
    
    * Fixed lint

--- cypress/integration/new_user_and_camera.js
+++ cypress/integration/new_user_and_camera.js
@@ -6,6 +6,7 @@ context('New User', () => {
   const username = "integrationtester"  +  uniqueId;
   const password = 'p' + username;
   const group = 'test' + uniqueId;
+  const camera = 'fake-' + group;
 
   beforeEach(() => {
     cy.visit('');
@@ -18,24 +19,30 @@ context('New User', () => {
     cy.contains("Email").siblings().type(username + "@fake.address.com");
     cy.contains("Password").siblings().type(password);
     cy.contains("Retype password").siblings().type(password);
+    cy.contains("I agree to the terms").click();
     cy.get("button").contains("Register").click();
+    cy.location({timeout: 60000}).should((location) => {expect(location.pathname).to.equal('/');});
 
     // create new group
-    cy.location({timeout: 60000}).should((location) => {expect(location.pathname).to.equal('/');});
     cy.contains("It looks like you're new here");
     cy.contains("Create a group").click();
+    cy.contains("Your groups");
+    cy.contains("Create group").click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);  // not ideal but popup is causing problems.  To fix later
     cy.contains("Group name").parent().type(group);
-    cy.contains("Create new group").click();
+    cy.contains("Group name").parent().type('{enter}');
 
     // check new group is created properly
     cy.location({timeout: 60000}).should((location) => {expect(location.pathname).contains(group);});
-    cy.contains('Group: ' + group);
-    // user is an administrator for group
-  });
+    cy.contains(group);
+    // user is an administrator for group - this can be done in web tests.
 
-  it('New user can sign in and create a group', () => {
-    cy.signin(username, password);
+    // create a camera in the group
+    cy.request("http://localhost:2040/create/" + camera + "?group-name=" + group);
+
+    // check that user can see camera
+    cy.visit('devices');
+    cy.contains(camera);
   });
 });
-
-
