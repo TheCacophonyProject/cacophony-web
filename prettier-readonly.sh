306cfd84132832efad56bd1bb377c8644fb602ab
Jon Hardie <jon@hardiesoft.com>
2021-07-26 11:32:27 +1200

    User prettier --check

--- prettier-readonly.sh
+++ prettier-readonly.sh
@@ -1,6 +1,6 @@
 # Run prettier but don't write changes
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  find -E . -iregex ".*\.(ts|js)" | egrep -v "(./node_modules/*|./apidoc*)" | xargs npx prettier
+  find -E . -iregex ".*\.(ts|js)" | egrep -v "(./node_modules/*|./apidoc*)" | xargs npx prettier --check
 else
-  find . -regextype posix-extended -regex '.*\.(ts|js)$' | egrep -v "(./node_modules/*|./apidoc*)" | xargs npx prettier
+  find . -regextype posix-extended -regex '.*\.(ts|js)$' | egrep -v "(./node_modules/*|./apidoc*)" | xargs npx prettier --check
 fi
