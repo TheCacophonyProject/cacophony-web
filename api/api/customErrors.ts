b918e255fa0a9990c097dee9758cfa6aa0e6d985
jon <jon@hardiesoft.com>
2021-12-16 13:53:22 +1300

    Try to fix issue with power-tagger tracks (3)

--- api/api/customErrors.ts
+++ api/api/customErrors.ts
@@ -19,10 +19,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import log from "../logging";
 import { format } from "util";
 import { asyncLocalStorage } from "@/Globals";
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function errorHandler(err: Error, request: Request, response: Response, next: NextFunction) {
+function errorHandler(
+  err: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
   if (
     err instanceof SyntaxError &&
     (err as any).type === "entity.parse.failed"
