92d82b8e8c42ceed4418490d455512bc79d7cbc9
Patrick <patrickb321@gmail.com>
2022-01-20 18:02:59 +1300

    refactor(migration): change use of sequelize models to use only SQL queries
    Was able to access the Models in the Travis CI, so reverted to only SQL. Generally not a good idea to use the models in migrations anyhow.

--- api/migrations/20220110021448-cacophonyIndex-to-Recordings.js
+++ api/migrations/20220110021448-cacophonyIndex-to-Recordings.js
@@ -1,12 +1,8 @@
 "use strict";
-const AllModels = require("../models");
-const { Recording, Track, DetailSnapshot, TrackTag } = AllModels.default;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const Op = Sequelize.Op;
     const transaction = await queryInterface.sequelize.transaction();
-
     try {
       // chcek recordings has column cacophonyIndex
       const hasColumn = await queryInterface.describeTable("Recordings");
@@ -17,65 +13,85 @@ module.exports = {
           Sequelize.JSONB
         );
       }
-      // converts cacophony_index in additionalMetadata to cacophonyIndex
-      const recordings = await Recording.findAll({
-        transaction,
-        where: {
-          fileMimeType: {
-            [Op.eq]: "audio/mp3",
-          },
-          additionalMetadata: {
-            // prettier-ignore
-            [Op.contains]: {analysis: {}},
-          },
-        },
-      });
 
-      const algorithm = await DetailSnapshot.getOrCreateMatching(
-        "algorithm",
-        {
-          algorithm: "sliding_window",
-        },
+      // sql get or create new algorithm into DetailSnapshot if does not exist
+      let algorithmId = await queryInterface.sequelize.query(`
+        SELECT "id" from "DetailSnapshots" 
+        WHERE "type"='algorithm' AND "details"->>'algorithm'='sliding_window';`);
+      if (!algorithmId) {
+        console.log("inserting new algorithm");
+        algorithmId = await queryInterface.sequelize.query(
+          `
+          INSERT INTO "DetailSnapshots" ("type", "details", "createdAt", "updatedAt")
+          VALUES ('algorithm', '{"algorithm":"sliding_window"}', NOW(), NOW())
+          RETURNING "id";`,
+          { transaction }
+        );
+      }
+      algorithmId = algorithmId[0][0].id;
+
+      const analysedRecordings = await queryInterface.sequelize.query(
+        `
+        SELECT "id", "additionalMetadata"->'analysis'->'species_identify' as speciesindentify FROM "Recordings"
+        WHERE "fileMimeType"='audio/mp3' AND "additionalMetadata"->'analysis'->'species_identify' IS NOT NULL
+        `,
         { transaction }
       );
 
-      await Promise.all(
-        recordings.map(async (recording) => {
-          const { id, additionalMetadata } = recording;
-          const { analysis, ...metaData } = additionalMetadata;
-          const { species_identify, cacophony_index } = analysis;
-          species_identify.forEach(
-            async ({ end_s, begin_s, species, liklihood }) => {
-              const newTrack = await Track.create(
-                {
-                  data: {
-                    start_s: begin_s,
-                    end_s: end_s,
-                  },
-                  RecordingId: id,
-                  AlgorithmId: algorithm.id,
-                },
-                { transaction }
-              );
-              await newTrack.addTag(species, liklihood, true, {
-                name: "Master",
-              });
-            }
-          );
+      console.log(analysedRecordings);
 
-          return await Recording.update(
-            {
-              additionalMetadata: metaData,
-              cacophonyIndex: cacophony_index,
-            },
-            {
-              where: {
-                id,
-              },
-              transaction,
-            }
+      await Promise.all(
+        analysedRecordings[0].map(async ({ id, speciesindentify }) => {
+          if (!speciesindentify) {
+            return;
+          }
+          return await Promise.all(
+            speciesindentify.map(
+              async ({ end_s, begin_s, species, liklihood }) => {
+                // Create a new track in Track table
+                const trackId = (
+                  await queryInterface.sequelize.query(
+                    `
+              INSERT INTO "Tracks" ("RecordingId", "AlgorithmId", "data", "createdAt", "updatedAt")
+              VALUES (${id}, ${algorithmId}, '{"start_s": ${begin_s}, "end_s": ${end_s}}', NOW(), NOW())
+              RETURNING "id";`,
+                    { transaction }
+                  )
+                )[0][0].id;
+                console.log(trackId);
+                return await queryInterface.sequelize.query(
+                  `
+              INSERT INTO "TrackTags" ("what", "confidence", "automatic", "data", "createdAt", "updatedAt", "TrackId")
+              VALUES ('${species}', ${liklihood}, true, '{"name": "Master"}', NOW(), NOW(), ${trackId})
+              `,
+                  { transaction }
+                );
+              }
+            )
           );
         })
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Recordings"
+        SET "cacophonyIndex" = "additionalMetadata"->'analysis'->'cacophony_index'
+        WHERE "fileMimeType" = 'audio/mp3'
+        AND "additionalMetadata"->'analysis'->'cacophony_index' IS NOT NULL
+        `,
+        { transaction }
+      );
+
+      console.log("modifying additionalMetadata");
+      // remove cacophony_index and species_identify from additionalMetadata
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Recordings"
+        SET "additionalMetadata" = ("additionalMetadata"||("additionalMetadata"->'analysis')-'cacophony_index'-'species_identify')-'analysis'
+        WHERE "fileMimeType" = 'audio/mp3'
+        AND "additionalMetadata"->'analysis'->'cacophony_index' IS NOT NULL
+        `,
+        { transaction }
       );
       await transaction.commit();
     } catch (error) {
@@ -84,72 +100,75 @@ module.exports = {
     }
   },
   down: async (queryInterface, Sequelize) => {
-    const Op = Sequelize.Op;
+    // const Op = Sequelize.Op;
     const transaction = await queryInterface.sequelize.transaction();
     try {
-      const hasColumn = await queryInterface.describeTable("Recordings");
-      if (hasColumn.cacophonyIndex) {
-        // Get All recordings with cacophonyIndex
-        const recordings = await Recording.findAll({
-          transaction,
-          where: {
-            fileMimeType: {
-              [Op.eq]: "audio/mp3",
-            },
-            cacophonyIndex: {
-              [Op.ne]: null,
-            },
-          },
-        });
-        await Promise.all(
-          recordings.map(async (recording) => {
-            const { id, cacophonyIndex, additionalMetadata } = recording;
-            const tracks = await Track.findAll({
-              transaction,
-              where: {
-                RecordingId: id,
-              },
-            });
-            const species_identify = await Promise.all(
-              tracks.map(async (track) => {
-                const { id } = track;
-                const tags = await TrackTag.findAll({
-                  transaction,
-                  where: {
-                    TrackId: id,
-                  },
-                });
-                await track.destroy({ transaction });
-                return await Promise.all(
-                  tags.map(async (tag) => {
-                    tag.destroy({ transaction });
-                    return {
-                      liklihood: tag.confidence,
-                      end_s: track.data.end_s,
-                      begin_s: track.data.start_s,
-                      species: tag.what,
-                      who: tag.data.name,
-                    };
-                  })
+      const analysedRecordings = await queryInterface.sequelize.query(
+        `
+        SELECT "id", "cacophonyIndex" FROM "Recordings"
+        WHERE "fileMimeType"='audio/mp3' AND "cacophonyIndex" IS NOT NULL
+        `,
+        { transaction }
+      );
+      console.log(analysedRecordings);
+
+      await Promise.all(
+        analysedRecordings[0].map(
+          async ({ id: recordingId, cacophonyIndex }) => {
+            const tracks = await queryInterface.sequelize.query(
+              `
+            SELECT "id", "data" FROM "Tracks"
+            WHERE "RecordingId"=${recordingId}
+            `,
+              { transaction }
+            );
+            console.log(tracks);
+            const speciesIdentify = await Promise.all(
+              tracks[0].map(async ({ id: trackId, data }) => {
+                const { start_s, end_s } = data;
+                const tags = await queryInterface.sequelize.query(
+                  `
+                SELECT "what", "confidence" FROM "TrackTags"
+                WHERE "TrackId"=${trackId}
+                `,
+                  { transaction }
                 );
+                console.log(tags);
+                const speciesIndentify = tags[0].map(({ what, confidence }) => {
+                  const species_identify = {
+                    begin_s: start_s,
+                    end_s: end_s,
+                    species: what,
+                    liklihood: confidence,
+                  };
+                  return species_identify;
+                });
+                return speciesIndentify;
               })
             );
-            const prevAdditionalMetadata = {
-              ...additionalMetadata,
-              analysis: {
-                species_identify: species_identify.flat(2),
-                cacophony_index: cacophonyIndex,
-              },
-            };
-            return await recording.update({
-              additionalMetadata: prevAdditionalMetadata,
+            const sql = `
+                    UPDATE "Recordings"
+                    SET "additionalMetadata" = ("additionalMetadata" ||
+                      jsonb_build_object('analysis',
+                        jsonb_build_object(
+                          'cacophony_index_version', "additionalMetadata"->'cacophony_index_version',
+                          'processing_time_seconds', "additionalMetadata"->'processing_time_seconds',
+                          'species_identify_version', "additionalMetadata"->'species_identify_version'
+                        ) || 
+                        '{"cacophony_index": ${JSON.stringify(
+                          cacophonyIndex
+                        )}, "species_identify": ${JSON.stringify(
+              speciesIdentify.flat(2)
+            )}}'
+                      ))-'cacophony_index_version'-'processing_time_seconds'-'species_identify_version'
+                    WHERE "id"=${recordingId}
+                  `;
+            return await queryInterface.sequelize.query(sql, {
+              transaction,
             });
-          })
-        );
-        await queryInterface.removeColumn("Recordings", "cacophonyIndex", {
-          transaction,
-        });
-      }
+          }
+        )
+      );
       await transaction.commit();
     } catch (error) {
       await transaction.rollback();
