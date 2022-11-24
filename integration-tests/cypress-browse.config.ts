c846cc88a00964f7d92733b8b4c69b5715280086
jon <jon@hardiesoft.com>
2022-11-24 17:33:08 +1300

    - Adding browser e2e tests for browse-next signup and login + group membership flows (2)

--- integration-tests/cypress-browse.config.ts
+++ integration-tests/cypress-browse.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'dyez6t',
+  projectId: "dyez6t",
   env: {
-    'cacophony-api-server': 'http://localhost:1080',
+    "cacophony-api-server": "http://localhost:1080",
     testCreds: {},
   },
   chromeWebSecurity: false,
@@ -11,9 +11,9 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    specPattern: 'cypress/e2e/browse-next/**/*.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:5173',
+    specPattern: "cypress/e2e/browse-next/**/*.{js,jsx,ts,tsx}",
+    baseUrl: "http://localhost:5173",
   },
-})
+});
