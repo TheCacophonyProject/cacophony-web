1f77e953cb13670516941de54819d9b3d90b80af
Will Georgeson <wge23@uclive.ac.nz>
2023-12-06 10:03:55 +1300

    Project preferences settings basic structure

--- browse-next/src/views/UserGroupPreferencesView.vue
+++ browse-next/src/views/UserGroupPreferencesView.vue
@@ -14,9 +14,12 @@ const preferencesModalEnabled = ref<boolean>(true);
 const initialSpecies = ['Possum', 'Rat', 'Cat'];
 const speciesArray = ref<string[]>(initialSpecies);
 const preferencesModalRef = ref<any>(null);
-const preferenceNavigationItems = ['Notifications', 'Media', 'Tagging'];
+const preferenceNavigationItems = ['Notifications', 'Media', 'Tagging', 'Species'];
 const preferenceNavigationItemsArray = ref<string[]>(preferenceNavigationItems);
-
+const emailDailyDigestSwitch = ref(true);
+const emailVisitEventSwitch = ref(true);
+const pushDailyDigestSwitch = ref(true);
+const pushVisitEventSwitch = ref(true);
 
 function togglePreferencesModal() {
   preferencesModalEnabled.value = !preferencesModalEnabled.value;
@@ -52,107 +55,199 @@ function getSpeciesIcon(species: string) {
 
 </script>
 <template>
-  <div class="pageContent">
-    <div class="preferencesNavigation">
-      <section-header>Project preferences</section-header>
-      <nav>
-        <ul>
-          <li v-for="(item, index) in preferenceNavigationItemsArray" :key="index">
-            <a href="#" class="navItem">
-              <font-awesome-icon
-                v-if="index === 0"
-                :icon="['fas', 'envelope']"
-                class="navIcon"
-              />
-              <font-awesome-icon
-                v-else-if="index === 1"
-                :icon="['fas', 'camera']"
-                class="navIcon"
-              />
-              <font-awesome-icon
-                v-else-if="index === 2"
-                :icon="['fas', 'tag']"
-                class="navIcon"
-              />
-              {{ item }}
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <div class="preferencesContent">
-      <h6>Things that could appear here:</h6>
-      <ul>
-        <li>My project/location alert settings</li>
-        <li>Prefer video or audio views by default?</li>
-        <li>My preferred tags for video, audio</li>
-      </ul>
-
-      <ul v-if="alerts.length">
-        <li v-for="alert in alerts" :key="alert.id">{{ alert }}</li>
-      </ul>
-      <leave-project-modal v-model="selectedLeaveProject" />
-
-      <b-button v-b-modal.projectPreferencesModal class="preferencesModal" @click="openModal">Preferences Modal</b-button>
-
-      <div>
-        <b-modal id="projectPreferencesModal" hide-header hide-footer>
-          <div class="projectPreferencesContent">
-            <h5>What species would you like to monitor?</h5>
-            <div v-for="(species, index) in initialSpecies" :key="index" class="species-checkbox">
-              <span @click="toggleSpecies(species)" class="checkbox-icon">
+  <div>
+    <section-header>Project preferences</section-header>
+    <div class="pageContent">
+      <div class="preferencesNavigation">
+        <nav>
+          <ul>
+            <li v-for="(item, index) in preferenceNavigationItemsArray" :key="index">
+              <a href="#" class="navItem">
                 <font-awesome-icon
-                  :icon="getSpeciesIcon(species)"
-                  :class="{ checked: speciesArray.includes(species) }"
+                  v-if="index === 0"
+                  :icon="['fas', 'envelope']"
+                  class="navIcon"
                 />
-              </span>
-              <label>{{ species }}</label>
+                <font-awesome-icon
+                  v-else-if="index === 1"
+                  :icon="['fas', 'camera']"
+                  class="navIcon"
+                />
+                <font-awesome-icon
+                  v-else-if="index === 2"
+                  :icon="['fas', 'tag']"
+                  class="navIcon"
+                />
+                <font-awesome-icon 
+                  v-else-if="index === 3"
+                  :icon="['fas', 'cat']"
+                  class="navIcon"
+                />
+                {{ item }}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="preferencesContent">
+        <div class="notificationContent">
+          <h3>Notification Settings</h3>
+          <p style="color: #525252">Select the kinds of notifications you get about your project</p>
+          <div class="divider"></div>
+          <div class="emailNotifications">
+            <div class="emailHeading">
+              <h6>Email notifications</h6>
+              <p style="color: grey">Recieve emails to find out what's going on when you're not online</p>
             </div>
-            <div class="modal-buttons">
-              <button class="btn btn-primary">Save</button>
-              <button class="btn btn-secondary">Cancel</button>
+            <div class="emailSettings">
+              <div class="digestHeading">
+                <h6 class="digestTitle">Daily digest</h6>
+                <div class="digestSwitch">
+                  <b-form-checkbox v-model="emailDailyDigestSwitch" switch></b-form-checkbox>
+                </div>
+              </div>
+              <p style="color: grey">A daily overview of your project</p>
+              <div class="visitHeading">
+                <h6 class="visitTitle">Visit event</h6>
+                <div class="visitSwitch">
+                  <b-form-checkbox v-model="emailVisitEventSwitch" switch></b-form-checkbox>
+                </div>
+              </div>
+              <p style="color: grey">Recieve a notification upon every visit</p>
             </div>
           </div>
-        </b-modal>
-      </div>
+          <div class="divider"></div>
+          <div class="pushNotifications">
+            <div class="pushHeading">
+              <h6>Push notifications</h6>
+              <p style="color: grey">Recieve push notifications to find out what's going on when you're not online</p>
+            </div>
+            <div class="pushSettings">
+              <div class="digestHeading">
+                <h6 class="digestTitle">Daily digest</h6>
+                <div class="digestSwitch">
+                  <b-form-checkbox v-model="pushDailyDigestSwitch" switch></b-form-checkbox>
+                </div>
+              </div>
+              <p style="color: grey">A daily overview of your project</p>
+              <div class="visitHeading">
+                <h6 class="visitTitle">Visit event</h6>
+                <div class="visitSwitch">
+                  <b-form-checkbox v-model="pushVisitEventSwitch" switch></b-form-checkbox>
+                </div>
+              </div>
+              <p style="color: grey">Recieve a notification upon every visit</p>
+            </div>
+          </div>
+        </div>
 
-      <button
-        class="btn btn-outline-danger"
-        type="button"
-        @click="selectedLeaveProject = true"
-        v-if="!isNotOnlyProjectOwnerOrAdmin"
-      >
-        Leave this project
-      </button>
+        <div class="mediaContent">
+          <h3>Media Preferences</h3>
+          <p>Default media</p>
+        </div>
+
+         <div class="speciesContent">
+          <h3>Species Preferences</h3>
+          <b-button v-b-modal.projectPreferencesModal class="preferencesModal" @click="openModal">Species preferences</b-button>
+        </div>
+
+         <div class="taggingContent">
+          <h3>Tagging Preferences</h3>
+          <p>My preferred tags for video, audio </p>
+        </div>
+
+        <ul v-if="alerts.length">
+          <li v-for="alert in alerts" :key="alert.id">{{ alert }}</li>
+        </ul>
+        <leave-project-modal v-model="selectedLeaveProject" />
+
+        <div>
+          <b-modal id="projectPreferencesModal" hide-header hide-footer>
+            <div class="projectPreferencesContent">
+              <h5>What species would you like to monitor?</h5>
+              <div v-for="(species, index) in initialSpecies" :key="index" class="species-checkbox">
+                <span @click="toggleSpecies(species)" class="checkbox-icon">
+                  <font-awesome-icon
+                    :icon="getSpeciesIcon(species)"
+                    :class="{ checked: speciesArray.includes(species) }"
+                  />
+                </span>
+                <label>{{ species }}</label>
+              </div>
+              <div class="modal-buttons">
+                <button class="btn btn-primary">Save</button>
+                <button class="btn btn-secondary">Cancel</button>
+              </div>
+            </div>
+          </b-modal>
+        </div>
+
+        <button
+          class="btn btn-outline-danger"
+          type="button"
+          @click="selectedLeaveProject = true"
+          v-if="!isNotOnlyProjectOwnerOrAdmin"
+        >
+          Leave this project
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.digestHeading,
+.visitHeading {
+  width: 100%;
+  display: flex;
+}
+
+.digestTitle,
+.visitTitle {
+  flex: 20;
+}
+
+.disgestSwitch,
+.visitSwitch {
+  flex: 1;
+}
+
+.emailNotifications,
+.pushNotifications {
+  display: flex;
+}
+
+.emailHeading,
+.pushHeading {
+  flex: 1;
+}
+
+.emailSettings,
+.pushSettings {
+  padding-left: 5em;
+  flex: 2;
+}
+
+.divider {
+  padding: 0.5em 0;
+  border-top: 1.2px solid #d8d8d8;
+}
+
 .section-header {
   padding-left: 1em;
 }
 .preferencesNavigation {
   background-color: #283447;
-  overflow-y: auto;
-  padding-top: 20px;
+  border-radius: 0.5em;
+  padding: 0.5em 0px;
   flex: 1;
+  /* height: auto; */
 }
 
 nav ul {
   list-style-type: none;
   padding: 0;
 }
-
-/* .navItem {
-  color: #ffffff;
-  display: block;
-  padding: 12px 20px;
-  text-decoration: none;
-  transition: background-color 0.3s ease;
-  margin-left: 0.5em;
-  margin-right: 0.5em;
-} */
 
 .navItem:hover {
   background-color: #1a2430;
@@ -174,26 +269,40 @@ nav ul {
 }
 
 .navIcon {
+  width: 24px;
   margin-right: 1.2em; /* Spacing between icon and label */
   color: white; /* Icon color */
 }
 
+.notificationContent,
+.mediaContent,
+.speciesContent,
+.taggingContent {
+  background-color:rgb(231, 230, 230);
+  border-radius: 0.5em;
+  margin-top: 0.7em;
+  margin-left: 0.7em;
+  padding-top: 1.7em;
+  padding-left: 1.7em;
+  padding-right: 1.7em;
+  padding-bottom: 1em;
+}
+
+.notificationContent {
+  margin-top: 0em;
+}
 .preferencesContent {
-  padding-top: 1em;
-  padding-left: 2em;
-  /* background-color: pink; */
   flex: 3;
 }
 .pageContent {
   /* background-color: rgb(239, 239, 186); */
   width: 100%;
-  height: 100vh;
   display: flex;
 }
 .projectPreferencesContent {
   background-color: #fff;
   border-radius: 8px;
-  padding: 20px;
+  /* padding: 20px; */
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
