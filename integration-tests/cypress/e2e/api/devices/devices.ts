b5b8c504d6a11a7e7698afd54e21e614cac1dc3a
Will Georgeson <wge23@uclive.ac.nz>
2023-12-12 09:39:51 +1300

    Added in a location setting device

--- integration-tests/cypress/e2e/api/devices/devices.ts
+++ integration-tests/cypress/e2e/api/devices/devices.ts
@@ -1,8 +1,35 @@
 /// <reference path="../../../support/index.d.ts" />
 import { getTestName } from "@commands/names";
 import { makeAuthorizedRequest, v1ApiPath, getCreds } from "@commands/server";
+import { TestGetLocation } from "@commands/api/station";
+import { TestCreateExpectedHistoryEntry } from "@commands/api/device";
 import ApiDeviceResponse = Cypress.ApiDeviceResponse;
 import { DeviceType } from "@typedefs/api/consts";
+import {
+  TEMPLATE_THERMAL_RECORDING,
+  TEMPLATE_THERMAL_RECORDING_RESPONSE,
+} from "@commands/dataTemplate";
+import { DeviceHistoryEntry, TestNameAndId } from "@commands/types";
+import { ApiThermalRecordingResponse } from "@typedefs/api/recording";
+import { NOT_NULL, NOT_NULL_STRING } from "@commands/constants";
+
+
+const templateExpectedCypressRecording: ApiThermalRecordingResponse =
+  JSON.parse(JSON.stringify(TEMPLATE_THERMAL_RECORDING_RESPONSE));
+
+const templateExpectedStation = {
+  location,
+  name: NOT_NULL_STRING,
+  id: NOT_NULL,
+  lastThermalRecordingTime: NOT_NULL_STRING,
+  createdAt: NOT_NULL_STRING,
+  updatedAt: NOT_NULL_STRING,
+  activeAt: NOT_NULL_STRING,
+  automatic: true,
+  needsRename: true,
+  groupId: NOT_NULL,
+  groupName: NOT_NULL_STRING,
+};
 
 describe("Devices list", () => {
   const superuser = getCreds("superuser")["email"];
@@ -21,6 +48,12 @@ describe("Devices list", () => {
   const camera2 = "second_E_camera";
   const camera3 = "Charlie-camera";
   const camera4 = "Debbie-camera";
+  const Josie = "Josie_recordings_stations";
+  const group4 = "recordings_stations";
+  const group5= "recordings_stations-2";
+
+
+
   let expectedDeviceAdminView: ApiDeviceResponse;
   let expectedDeviceMemberView: ApiDeviceResponse;
   let expectedDevice2AdminView: ApiDeviceResponse;
@@ -52,6 +85,15 @@ describe("Devices list", () => {
         type: DeviceType.Unknown,
       };
     });
+
+    cy.testCreateUserAndGroup(Josie, group4).then(() => {
+      templateExpectedCypressRecording.groupId = getCreds(group4).id;
+      templateExpectedCypressRecording.groupName = getTestName(group4);
+      templateExpectedStation.groupId = getCreds(group4).id;
+      templateExpectedStation.groupName = getTestName(group4);
+    });
+    cy.apiGroupAdd(Josie, group5);
+
     cy.apiGroupUserAdd(groupAdmin, groupMember, group, NOT_ADMIN);
 
     //second group
@@ -159,7 +201,102 @@ describe("Devices list", () => {
     it.skip("Super-user 'as user' should see only their devices and users only where they are device admin", () => {});
   }
 
-  it("Set, retrieve, and validate a mask region for the latest device location", () => {
+  it("Setting a mask region if the device has a current location", () => {
+    const deviceName = "new-device";
+    const recordingTime = new Date(
+      new Date().setDate(new Date().getDate() + 1)
+    );
+    const location = TestGetLocation(1);
+    const expectedStation1 = JSON.parse(
+      JSON.stringify(templateExpectedStation)
+    );
+    const expectedHistory: DeviceHistoryEntry[] = [];
+
+    expectedStation1.location = location;
+    expectedStation1.activeAt = recordingTime.toISOString();
+    expectedStation1.lastThermalRecordingTime = recordingTime.toISOString();
+    cy.apiDeviceAdd(deviceName, group4).then(() => {
+      cy.log("Add a recording and check new station is created");
+      cy.testUploadRecording(deviceName, {
+        ...location,
+        time: recordingTime,
+        noTracks: true,
+      })
+        .thenCheckStationIsNew(Josie)
+        .then((station: TestNameAndId) => {
+          cy.log("Check station created correctly");
+          cy.apiStationCheck(Josie, station.name, expectedStation1);
+
+          cy.log("Check device has a new valid deviceHistory created");
+          expectedHistory[0] = TestCreateExpectedHistoryEntry(
+            deviceName,
+            group4,
+            NOT_NULL_STRING,
+            null,
+            "register",
+            null
+          );
+          expectedHistory[1] = TestCreateExpectedHistoryEntry(
+            deviceName,
+            group4,
+            recordingTime.toISOString(),
+            location,
+            "automatic",
+            station.name
+          );
+          cy.apiDeviceHistoryCheck(Josie, deviceName, expectedHistory);
+        });
+    });
+  });
+
+
+  it("Set, retrieve, and validate a single mask region for the latest device location", () => {
+    const id = getCreds(camera2).id;
+    let getResponse;
+    const testRegions = [
+      {
+        region: "0",
+        points: [
+          { x: 0.99, y: 0.66 },
+          { x: 0.80, y: 0.83 },
+          { x: 0.58, y: 0.18 },
+          { x: 0.3, y: 0.1 },
+          { x: 0.5, y: 0.7 },
+          { x: 0.8, y: 0.4 },
+          { x: 0.9, y: 0.3 },
+          { x: 0.1, y: 0.02 },
+          { x: 0.12, y: 0.3}
+        ]
+      },
+    ];
+    makeAuthorizedRequest(
+      {
+        method: "POST",
+        url: v1ApiPath(`devices/${id}/mask-regions`, id),
+        body: {
+          "maskRegions": [
+            testRegions
+          ]
+        },
+      },
+      user2
+      );
+
+      makeAuthorizedRequest(
+        {
+          method: "GET",
+          url: v1ApiPath(`devices/${id}/mask-regions`, id),
+        },
+        user2
+      ).then((response) => {
+        getResponse = response.body.maskRegions;
+        const postRegionPoints = testRegions;
+        const getRegionPoints = getResponse[0];
+        expect(postRegionPoints).to.deep.equal(getRegionPoints);
+      });
+  });
+
+  it("Set, retrieve, and validate multiple mask regions for the latest device location", () => {
     const id = getCreds(camera2).id;
     let getResponse;
     const testRegions = [
