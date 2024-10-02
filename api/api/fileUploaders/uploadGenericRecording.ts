aacfa3ad297e95388774ee83a16c6f05859a5899
jon <jon@hardiesoft.com>
2024-10-03 11:10:55 +1300

    - Only set device active when getting a new recording uploaded via sidekick if it's currently inactive, and there's no active instance of that device elsewhere in the system.

--- api/api/fileUploaders/uploadGenericRecording.ts
+++ api/api/fileUploaders/uploadGenericRecording.ts
@@ -652,14 +652,29 @@ export const uploadGenericRecording =
           recordingDevice.lastConnectionTime ||
           !recordingDevice.lastConnectionTime)
       ) {
+        let shouldSetActive = false;
+        if (!recordingDevice.active) {
+          // Check if the device has been re-assigned to another group:
+          const activeDevice = models.Device.findOne({
+            where: {
+              saltId: recordingDevice.saltId,
+              active: true,
+            },
+          });
+          if (!activeDevice) {
+            shouldSetActive = true;
+          }
+        }
         // If we're getting a recording via sidekick that's later than a previous lastConnectionTime,
         // or there is no previous lastConnectionTime, we can null out the lastConnectionTime,
         // which indicates that this device is now "offline".
         // As such, it will no longer be targeted by stopped device emails, and can show up as offline in browse.
         recordingDeviceUpdatePayload = {
           lastConnectionTime: null,
-          active: true,
         };
+        if (shouldSetActive) {
+          (recordingDeviceUpdatePayload as any).active = true;
+        }
       }
 
       const wouldHaveSuppliedTracks = dataHasSuppliedTracks(data);
