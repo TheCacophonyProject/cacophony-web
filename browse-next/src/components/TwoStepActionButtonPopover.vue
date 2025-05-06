cde746d0e0327b877b009c587896e2a2d5907741
jon <jon@hardiesoft.com>
2025-05-06 12:55:01 +1200

    - Fix space in recording action icons due to two-stage-action-button.

--- browse-next/src/components/TwoStepActionButtonPopover.vue
+++ browse-next/src/components/TwoStepActionButtonPopover.vue
@@ -1,41 +1,43 @@
 <template>
-  <b-popover
-    click
-    :disabled="disabled"
-    :placement="placement"
-    no-fade
-    :strategy="'fixed'"
-    :delay="{ show: 0, hide: 0 }"
-    :boundary-padding="{ top: 17, bottom: 17 }"
-    :close-on-hide="hasBoundaryPadding"
-    ref="popover"
-    @hidden="didHide"
-  >
-    <template #target>
-      <button class="btn" :class="[...classes]" @click.stop.prevent="() => {}">
-        <font-awesome-icon
-          :icon="icon"
-          v-if="icon"
-          :color="color || 'inherit'"
-          :rotation="rotate || null"
-        />
-        <span v-if="computedLabel" class="ps-2" v-html="computedLabel" />
-      </button>
-    </template>
-
-    <button
-      @click.stop.prevent="
-        () => {
-          action();
-          shouldHideInternal = true;
-        }
-      "
-      class="btn btn-outline-danger text-nowrap w-100"
+  <div>
+    <b-popover
+      click
+      :disabled="disabled"
+      :placement="placement"
+      no-fade
+      :strategy="'fixed'"
+      :delay="{ show: 0, hide: 0 }"
+      :boundary-padding="{ top: 17, bottom: 17 }"
+      :close-on-hide="hasBoundaryPadding"
+      ref="popover"
+      @hidden="didHide"
     >
-      <font-awesome-icon icon="exclamation-triangle" />
-      <span class="ms-2" v-html="computedConfirmationLabel" />
-    </button>
-  </b-popover>
+      <template #target>
+        <button class="btn" :class="[...classes]" @click.stop.prevent="() => {}">
+          <font-awesome-icon
+            :icon="icon"
+            v-if="icon"
+            :color="color || 'inherit'"
+            :rotation="rotate || null"
+          />
+          <span v-if="computedLabel" class="ps-2" v-html="computedLabel" />
+        </button>
+      </template>
+
+      <button
+        @click.stop.prevent="
+          () => {
+            action();
+            shouldHideInternal = true;
+          }
+        "
+        class="btn btn-outline-danger text-nowrap w-100"
+      >
+        <font-awesome-icon icon="exclamation-triangle" />
+        <span class="ms-2" v-html="computedConfirmationLabel" />
+      </button>
+    </b-popover>
+  </div>
 </template>
 <script setup lang="ts">
 import { computed, ref } from "vue";
