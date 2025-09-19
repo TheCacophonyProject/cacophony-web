94f9081f9dcc99dc12953496f3b09f63422653ab
gferraro <g.ferraro22@gmail.com>
2025-09-19 16:31:09 +1200

    added new indexes script

---
+++ api/migrations/20250919042218-event-indexes.cjs
@@ -0,0 +1,14 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+        await queryInterface.sequelize.query(`CREATE INDEX events_devicedatetime_idx ON public."Events" USING btree ("dateTime", "DeviceId")`);
+        await queryInterface.sequelize.query(`CREATE INDEX detailsnapshots_details_idx ON public."DetailSnapshots" USING hash (details)`);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`DROP INDEX events_devicedatetime_idx`);
+    await queryInterface.sequelize.query(`DROP INDEX detailsnapshots_details_idx`);
+  }
+};
