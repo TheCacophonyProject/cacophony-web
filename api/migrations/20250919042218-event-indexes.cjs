79032b3db2053fb36c52a27bdbf6fa0b899402d4
gferraro <g.ferraro22@gmail.com>
2025-09-22 15:27:02 +1200

    beautified

--- api/migrations/20250919042218-event-indexes.cjs
+++ api/migrations/20250919042218-event-indexes.cjs
@@ -3,6 +3,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+        await queryInterface.sequelize.query(`
+        DROP INDEX IF EXISTS events_datetime
+    `);
+            await queryInterface.sequelize.query(`
+        DROP INDEX IF EXISTS events_devicedatetime_idx
+    `);
+            await queryInterface.sequelize.query(`
+        DROP INDEX IF EXISTS detailsnapshots_details_idx
+    `);
         await queryInterface.sequelize.query(`CREATE INDEX events_devicedatetime_idx ON public."Events" USING btree ("dateTime", "DeviceId")`);
         await queryInterface.sequelize.query(`CREATE INDEX detailsnapshots_details_idx ON public."DetailSnapshots" USING hash (details)`);
   },
@@ -10,5 +19,8 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`DROP INDEX events_devicedatetime_idx`);
     await queryInterface.sequelize.query(`DROP INDEX detailsnapshots_details_idx`);
+    await queryInterface.addIndex("Events", ["dateTime"], {
+      name: "events_datetime",
+    });
   }
 };
