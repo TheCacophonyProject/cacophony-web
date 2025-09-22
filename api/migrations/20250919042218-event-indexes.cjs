77ee5fc7cdee85711f4b6cbfbbc0555618feea37
gferraro <g.ferraro22@gmail.com>
2025-09-22 15:37:56 +1200

    update event indexes

--- api/migrations/20250919042218-event-indexes.cjs
+++ api/migrations/20250919042218-event-indexes.cjs
@@ -1,26 +1,28 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-        await queryInterface.sequelize.query(`
-        DROP INDEX IF EXISTS events_datetime
-    `);
-            await queryInterface.sequelize.query(`
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
         DROP INDEX IF EXISTS events_devicedatetime_idx
     `);
-            await queryInterface.sequelize.query(`
+    await queryInterface.sequelize.query(`
         DROP INDEX IF EXISTS detailsnapshots_details_idx
     `);
-        await queryInterface.sequelize.query(`CREATE INDEX events_devicedatetime_idx ON public."Events" USING btree ("dateTime", "DeviceId")`);
-        await queryInterface.sequelize.query(`CREATE INDEX detailsnapshots_details_idx ON public."DetailSnapshots" USING hash (details)`);
+    await queryInterface.sequelize.query(
+      `CREATE INDEX events_devicedatetime_idx ON public."Events" USING btree ("dateTime", "DeviceId")`
+    );
+    await queryInterface.sequelize.query(
+      `CREATE INDEX detailsnapshots_details_idx ON public."DetailSnapshots" USING hash (details)`
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.sequelize.query(`DROP INDEX events_devicedatetime_idx`);
-    await queryInterface.sequelize.query(`DROP INDEX detailsnapshots_details_idx`);
-    await queryInterface.addIndex("Events", ["dateTime"], {
-      name: "events_datetime",
-    });
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(
+      `DROP INDEX events_devicedatetime_idx`
+    );
+    await queryInterface.sequelize.query(
+      `DROP INDEX detailsnapshots_details_idx`
+    );
+  },
 };
