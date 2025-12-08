d4927810127f37a2744be959dc195a77bfb3809e
jon <jon@hardiesoft.com>
2025-12-09 09:06:08 +1300

    - Fix reference image editor window resizing (2)

--- browse-next/src/components/DeviceSetupReferencePhoto.vue
+++ browse-next/src/components/DeviceSetupReferencePhoto.vue
@@ -496,10 +496,10 @@ const moveRevealHandle = (event: PointerEvent) => {
     const parentBounds = target.parentElement!.getBoundingClientRect();
     const handleBounds = target.getBoundingClientRect();
     const halfHandleWidth = handleBounds.width / 2;
-    const x = Math.min(
+    const x = Math.max(0, Math.min(
       event.clientX - parentBounds.left - revealGrabOffsetX,
       parentBounds.width,
-    );
+    ));
     const left = (x / parentBounds.width) * 100;
     if (revealSlider.value) {
       revealSlider.value.style.width = `${left}%`;
