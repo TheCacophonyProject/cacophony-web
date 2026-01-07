03869c54aef75b4731351d0b3736cd14e3fcd382
GP <g.ferraro22@gmail.com>
2026-01-08 12:40:31 +1300

    adjust visible attrs

--- browse-next/src/components/MapWithPoints.vue
+++ browse-next/src/components/MapWithPoints.vue
@@ -215,11 +215,11 @@ const mapLayers = [
     url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
     attribution:
       "Map data: &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors, <a href=\"http://viewfinderpanoramas.org\">SRTM</a> | Map style: &copy; <a href=\"https://opentopomap.org\">OpenTopoMap</a> (<a href=\"https://creativecommons.org/licenses/by-sa/3.0/\">CC-BY-SA</a>)",
-    visible: true,
+    visible: false,
   },
   {
     name: "OpenStreetMap Basemap",
-    visible: false,
+    visible: true,
     url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     attribution:
       "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors",
