3fcded9fb761659530a3af78afec981e155f7000
jon <jon@hardiesoft.com>
2026-02-19 14:06:27 +1300

    WIP - Fix tests

--- api/api/V1/Recording.ts
+++ api/api/V1/Recording.ts
@@ -190,7 +190,7 @@ export const mapTrack = (track: Track, minimal = false): ApiTrackResponse => {
   if (track.maxFreqHz !== null) {
     t.maxFreq = track.maxFreqHz;
   }
-  t.filtered = track.filtered || false;
+  t.filtered = track.filtered;
   if (
     !minimal &&
     track.data &&
