215677fece687cfcb0bafecf3c29cb7ae261fbb5
jon <jon@hardiesoft.com>
2026-05-14 14:32:06 +1200

    - Add retries to flakey test

--- integration-tests/cypress/e2e/api/batch-2/recordings/get-recordings-for-processing.ts
+++ integration-tests/cypress/e2e/api/batch-2/recordings/get-recordings-for-processing.ts
@@ -21,92 +21,95 @@ describe("Get recordings for processing", () => {
     }
   });
 
-  // eslint-disable-next-line cypress/no-async-tests
-  it("Can get recordings for processing in multiple states with one API call", async () => {
-    const project = await createProjectWithUserAndDevice();
-    const AdminUser = project.api();
-    const SuperAdminUser = project.api(project.getTestSuperUser());
+  it(
+    "Can get recordings for processing in multiple states with one API call",
+    { retries: 3 },
+    async () => {
+      const project = await createProjectWithUserAndDevice();
+      const AdminUser = project.api();
+      const SuperAdminUser = project.api(project.getTestSuperUser());
 
-    // Upload recordings and ensure that they are in the correct initial processing states
-    const [recA, recB] = await Promise.all([
-      uploadThermalRecordingFromDeviceForProject({
-        project,
-        recordingDateTime: new Date(),
-      }),
-      uploadThermalRecordingFromDeviceForProject({
-        project,
-        recordingDateTime: new Date(),
-        // NOTE: Adding supplied metadata_source should put this recording into "analyse" state.
-        metadata: {
-          algorithm: { name: "testAlgorithm" },
-          metadata_source: "test",
-        },
-      }),
-    ]);
-    const recordingA = (await AdminUser.Recordings.getRecordingById(
-      recA,
-    )) as ApiRecordingResponse;
-    const recordingB = (await AdminUser.Recordings.getRecordingById(
-      recB,
-    )) as ApiRecordingResponse;
-    expect(
-      recordingA.processingState,
-      "recording is in `trackAndAnalyse` state upon upload",
-    ).to.equal(RecordingProcessingState.TrackAndAnalyse);
-    expect(
-      recordingB.processingState,
-      "recording is in `analyse` state upon upload",
-    ).to.equal(RecordingProcessingState.Analyse);
-    // Two subsequent calls to Processing.get should return both recordings.
-    const processing1 =
-      await SuperAdminUser.Recordings.getOneRecordingForProcessing(
-        RecordingType.ThermalRaw,
-        [
-          RecordingProcessingState.TrackAndAnalyse,
-          RecordingProcessingState.AnalyseThermal,
-        ],
+      // Upload recordings and ensure that they are in the correct initial processing states
+      const [recA, recB] = await Promise.all([
+        uploadThermalRecordingFromDeviceForProject({
+          project,
+          recordingDateTime: new Date(),
+        }),
+        uploadThermalRecordingFromDeviceForProject({
+          project,
+          recordingDateTime: new Date(),
+          // NOTE: Adding supplied metadata_source should put this recording into "analyse" state.
+          metadata: {
+            algorithm: { name: "testAlgorithm" },
+            metadata_source: "test",
+          },
+        }),
+      ]);
+      const recordingA = (await AdminUser.Recordings.getRecordingById(
+        recA,
+      )) as ApiRecordingResponse;
+      const recordingB = (await AdminUser.Recordings.getRecordingById(
+        recB,
+      )) as ApiRecordingResponse;
+      expect(
+        recordingA.processingState,
+        "recording is in `trackAndAnalyse` state upon upload",
+      ).to.equal(RecordingProcessingState.TrackAndAnalyse);
+      expect(
+        recordingB.processingState,
+        "recording is in `analyse` state upon upload",
+      ).to.equal(RecordingProcessingState.Analyse);
+      // Two subsequent calls to Processing.get should return both recordings.
+      const processing1 =
+        await SuperAdminUser.Recordings.getOneRecordingForProcessing(
+          RecordingType.ThermalRaw,
+          [
+            RecordingProcessingState.TrackAndAnalyse,
+            RecordingProcessingState.AnalyseThermal,
+          ],
+        );
+      const processing2 =
+        await SuperAdminUser.Recordings.getOneRecordingForProcessing(
+          RecordingType.ThermalRaw,
+          [
+            RecordingProcessingState.TrackAndAnalyse,
+            RecordingProcessingState.AnalyseThermal,
+          ],
+        );
+      // The third call should return nothing to process
+      const processing3 =
+        await SuperAdminUser.Recordings.getOneRecordingForProcessing(
+          RecordingType.ThermalRaw,
+          [
+            RecordingProcessingState.TrackAndAnalyse,
+            RecordingProcessingState.AnalyseThermal,
+          ],
+        );
+      expect(processing3.status).to.equal(HttpStatusCode.OkNoContent);
+      const rec1 = processing1.result as {
+        recording: ApiRecordingResponse;
+        rawJWT: JwtToken<RecordingId>;
+      };
+      const rec2 = processing2.result as {
+        recording: ApiRecordingResponse;
+        rawJWT: JwtToken<RecordingId>;
+      };
+      expect(
+        rec1.recording.processingState,
+        "prioritise 'analyse' state",
+      ).to.equal(RecordingProcessingState.AnalyseThermal);
+      expect(rec2.recording.processingState).to.equal(
+        RecordingProcessingState.TrackAndAnalyse,
       );
-    const processing2 =
-      await SuperAdminUser.Recordings.getOneRecordingForProcessing(
-        RecordingType.ThermalRaw,
-        [
-          RecordingProcessingState.TrackAndAnalyse,
-          RecordingProcessingState.AnalyseThermal,
-        ],
-      );
-    // The third call should return nothing to process
-    const processing3 =
-      await SuperAdminUser.Recordings.getOneRecordingForProcessing(
-        RecordingType.ThermalRaw,
-        [
-          RecordingProcessingState.TrackAndAnalyse,
-          RecordingProcessingState.AnalyseThermal,
-        ],
-      );
-    expect(processing3.status).to.equal(HttpStatusCode.OkNoContent);
-    const rec1 = processing1.result as {
-      recording: ApiRecordingResponse;
-      rawJWT: JwtToken<RecordingId>;
-    };
-    const rec2 = processing2.result as {
-      recording: ApiRecordingResponse;
-      rawJWT: JwtToken<RecordingId>;
-    };
-    expect(
-      rec1.recording.processingState,
-      "prioritise 'analyse' state",
-    ).to.equal(RecordingProcessingState.AnalyseThermal);
-    expect(rec2.recording.processingState).to.equal(
-      RecordingProcessingState.TrackAndAnalyse,
-    );
 
-    cy.log("Check that we can download a file via signedUrl endpoint");
-    const recordingBinaryResponse =
-      await TestApiImpl.Recordings.getRecordingWithSignedUrl(rec1.rawJWT);
-    expect(recordingBinaryResponse.success, "Got recording with signed url").to
-      .be.true;
-    expect(recordingBinaryResponse.result).to.be.a("Blob");
-  });
+      cy.log("Check that we can download a file via signedUrl endpoint");
+      const recordingBinaryResponse =
+        await TestApiImpl.Recordings.getRecordingWithSignedUrl(rec1.rawJWT);
+      expect(recordingBinaryResponse.success, "Got recording with signed url")
+        .to.be.true;
+      expect(recordingBinaryResponse.result).to.be.a("Blob");
+    },
+  );
 
   it("Test priorities for processing queue", async () => {
     // TODO:
